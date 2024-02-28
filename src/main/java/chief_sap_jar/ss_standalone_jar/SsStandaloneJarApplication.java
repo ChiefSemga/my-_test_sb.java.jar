e0a50277b0351c6ada86333349e882ed49720cd2
ChiefSemga <n.semgaalex@yahoo.co.uk>
2024-02-28 08:59:45 +0100

    my first commit

---
+++ src/main/java/chief_sap_jar/ss_standalone_jar/SsStandaloneJarApplication.java
@@ -0,0 +1,13 @@
+package chief_sap_jar.ss_standalone_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsStandaloneJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsStandaloneJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chief_sap_jar/ss_standalone_jar/SsStandaloneJarApplicationTests.java
@@ -0,0 +1,13 @@
+package chief_sap_jar.ss_standalone_jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsStandaloneJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
